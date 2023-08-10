8d3eaf1dae65e4ab63115831dbca53c1bb1b0e57
es6 <benlin2003@gmail.com>
2023-08-10 01:35:05 -0400

    Updated HackProgramExercise to prompt user for PID and Address entry

--- HackProgramExercise/HackProgramExercise.cpp
+++ HackProgramExercise/HackProgramExercise.cpp
@@ -5,20 +5,33 @@ using namespace std;
 
 int main()
 {
+    DWORD pid = 0;
+    cout << "PID: ";
+    cin >> dec >> pid;
+
     int intRead = 0;
-    // Launch Hack Program first to get updated PID
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 27504);
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
     if (hProcess == NULL) {
         cout << "OpenProcess failed. GetLastError = " << dec << GetLastError() << endl;
         system("pause");
         return EXIT_FAILURE;
     }
 
-    // Retrieve current varInt address from Dummy Program
-    BOOL rpm = ReadProcessMemory(hProcess, (LPCVOID)0x33AF1BFB40, &intRead, sizeof(int), NULL);
+    // Prompt for address
+    uintptr_t address = 0x0;
+    cout << "Memory Address: 0x";
+    cin >> hex >> address;
+    getchar();
+
+    BOOL rpm = ReadProcessMemory(hProcess, (LPCVOID)address, &intRead, sizeof(int), NULL);
+    if (rpm == FALSE) {
+        cout << "ReadProcessMemory failed. GetLastError = " << dec << GetLastError() << endl;
+        system("pause");
+        return EXIT_FAILURE;
+    }
 
     cout << "intRead = " << dec << intRead << endl;
     cout << "Press ENTER to quit." << endl;
-    getchar();
+    system("pause > nul");
     return 0;
 }
