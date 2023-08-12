087f832de08b42bc8ae8da5f9be94daeeb1e766a
jamesgreen <benlin2003@gmail.com>
2023-08-11 21:39:35 -0400

    Completed initial WPMExercise

--- WPMExercise/WPMExercise.cpp
+++ WPMExercise/WPMExercise.cpp
@@ -5,5 +5,40 @@
 using namespace std;
 
 int main() {
-	return 0;
+	DWORD pid = 0;
+	cout << "PID: ";
+	cin >> dec >> pid;
+
+	HANDLE hProcess = OpenProcess(PROCESS_VM_WRITE, FALSE, pid);
+	if (hProcess == NULL) {
+		cout << "OpenProcess failed. GetLastError = " << GetLastError() << endl;
+		system("pause");
+		return EXIT_FAILURE;
+	}
+
+	uintptr_t varIntAddress = 0x0;
+	cout << "Enter varInt address: 0x";
+	cin >> hex >> varIntAddress;
+
+	int intToWrite = 0;
+	cout << "Enter integer to overwrite with: ";
+	cin >> dec >> intToWrite;
+
+	BOOL writeInt = WriteProcessMemory(hProcess, (LPVOID)varIntAddress, &intToWrite, sizeof(int), NULL);
+	if (!writeInt) {
+		cout << "WriteProcessMemory failed, GetLastError = " << GetLastError() << endl;
+		system("pause");
+		return EXIT_FAILURE;
+	}
+	cout << "Overwritten successfully" << endl;
+	
+	cout << "Press ENTER to quit." << endl;
+	BOOL closeHandle = CloseHandle(hProcess);
+	if (closeHandle == 0) {
+		cout << "CloseHandle failed, GetLastError() = " << GetLastError() << endl;
+		system("pause");
+		return EXIT_FAILURE;
+	}
+	system("pause > nul");
+	return EXIT_SUCCESS;
 }
